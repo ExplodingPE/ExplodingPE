7ea9cd28a8f9ed84bacc55263d1437f5f04e59f9
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-11 17:52:23 +0200

    #wip 2 you

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -922,9 +922,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 		$this->spawnToAll();
 
-		if($this->server->getUpdater()->hasUpdate() and $this->hasPermission(Server::BROADCAST_CHANNEL_ADMINISTRATIVE)){
-			$this->server->getUpdater()->showPlayerUpdate($this);
-		}
+		//if($this->server->getUpdater()->hasUpdate() and $this->hasPermission(Server::BROADCAST_CHANNEL_ADMINISTRATIVE)){
+		//	$this->server->getUpdater()->showPlayerUpdate($this);
+		//}
 
 		if($this->getHealth() <= 0){
 			$pk = new RespawnPacket();
