0401fc1d0c123f3091eb106d25f18e59d3d762c8
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-15 10:20:44 +0200

    Create travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -2,21 +2,6 @@
 
 PHP_BINARY="php"
 
-while getopts "p:" OPTION 2> /dev/null; do
-	case ${OPTION} in
-		p)
-			PHP_BINARY="$OPTARG"
-			;;
-	esac
-done
-
-./tests/lint.sh -p "$PHP_BINARY"
-
-if [ $? -ne 0 ]; then
-	echo Lint scan failed!
-	exit 1
-fi
-
 rm server.log 2> /dev/null
 mkdir -p ./plugins
 
@@ -26,7 +11,7 @@ cp -r tests/plugins/PocketMine-DevTools ./plugins
 rm -rf ./plugins/PocketMine-DevTools
 
 echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
-if ls plugins/DevTools/PocketMine*.phar >/dev/null 2>&1; then
+if ls plugins/DevTools/SpigotPE*.phar >/dev/null 2>&1; then
     echo Server phar created successfully.
 else
     echo No phar created!
