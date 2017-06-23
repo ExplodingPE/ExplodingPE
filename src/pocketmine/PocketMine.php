2fa7dbbd0128b3d3343e1aacea06e96ab158effa
BlawoHD <blawohd@gmail.com>
2017-06-23 16:41:06 +0200

    Remove 3.0.0-ALPHA6

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -79,7 +79,7 @@ namespace pocketmine {
 	use raklib\RakLib;
 
 	const VERSION = "1.3dev";
-	const API_VERSION = "3.0.0-ALPHA6";
+	const API_VERSION = "3.0.0";
 	const CODENAME = "Unleashed";
 
 	/*
