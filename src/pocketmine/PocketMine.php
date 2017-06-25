d87c58f9d9ba920989f0b72490b67712d1f6a6b4
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-25 09:30:14 +0200

    Add files via upload

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -78,7 +78,7 @@ namespace pocketmine {
 	use pocketmine\wizard\SetupWizard;
 	use raklib\RakLib;
 
-	const VERSION = "1.3dev";
+	const VERSION = "1.4dev";
 	const API_VERSION = "3.0.0";
 	const CODENAME = "Unleashed";
 
@@ -451,6 +451,7 @@ namespace pocketmine {
 			"mbstring" => "Multibyte String",
 			"yaml" => "YAML",
 			"sockets" => "Sockets",
+			"zip" => "Zip",
 			"zlib" => "Zlib"
 		];
 
@@ -492,6 +493,11 @@ namespace pocketmine {
 				$exitCode = -1;
 				break;
 			}
+		}
+
+
+		if(\Phar::running(true) === ""){
+			$logger->warning("Non-packaged PocketMine-MP installation detected, do not use on production.");
 		}
 
 		ThreadManager::init();
