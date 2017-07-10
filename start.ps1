c91e4e042c1eb0807ec74a869be9ffa5b6c1767b
Asuna-Yuuki <Wesleyvanneck1234@gmail.com>
2017-07-10 08:43:24 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -9,12 +9,12 @@ if(Test-Path "bin\php\php.exe"){
 	$binary = "php"
 }
 
-if(Test-Path "PocketMine-MP.phar"){
-	$file = "PocketMine-MP.phar"
+if(Test-Path "SpigotPE.phar"){
+	$file = "SpigotPE.phar"
 }elseif(Test-Path "src\pocketmine\PocketMine.php"){
 	$file = "src\pocketmine\PocketMine.php"
 }else{
-	echo "Couldn't find a valid PocketMine-MP installation"
+	echo "Couldn't find a valid SpigotPE installation"
 	pause
 	exit 1
 }
