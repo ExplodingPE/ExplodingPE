52f73c6a0337e6d30a960147757d0176d8529855
Fabiansigala <Fabiansigala@yahoo.com>
2017-08-06 14:21:22 -0700

    Should fix i think

--- src/pocketmine/utils/VersionString.php
+++ src/pocketmine/utils/VersionString.php
@@ -40,9 +40,9 @@ class VersionString{
 			$this->generation = ($version >> 9) & 0x0F;
 		}else{
 			$version = preg_split("/([A-Za-z]*)[ _\\-]?([0-9]*)\\.([0-9]*)\\.{0,1}([0-9]*)(dev|)(-[\\0-9]{1,}|)/", $version, -1, PREG_SPLIT_DELIM_CAPTURE);
-			$this->generation = isset($version[2]) ? (int) $version[2] : 0; //0-15
-			$this->major = isset($version[3]) ? (int) $version[3] : 0; //0-15
-			$this->minor = isset($version[4]) ? (int) $version[4] : 0; //0-31
+			$this->generation = (int) ($version[2] ?? 0); //0-15
+			$this->major = (int) ($version[3] ?? 0); //0-15
+			$this->minor = (int) ($version[4] ?? 0); //0-31
 			$this->development = $version[5] === "dev";
 			if($version[6] !== ""){
 				$this->build = (int) (substr($version[6], 1));
